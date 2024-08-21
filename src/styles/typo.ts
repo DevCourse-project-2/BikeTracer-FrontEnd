421b69b72a84be1ee69afdd3a3123e1f355337f1
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-08-22 06:26:34 +0900

    chore: 기본 디자인 시스템 구현 (#2)
    * feat: 전역 스타일 및 Pretendard 글꼴 설정
    
    * feat: 기본 typo 설정
    
    * feat: 기본 color 설정

---
+++ src/styles/typo.ts
@@ -0,0 +1,58 @@
+const typo = {
+  Main: {
+    SemiBold: {
+      fontFamily: 'Pretendard',
+      fontSize: '18px',
+      fontWeight: 700,
+      lineHeight: '1.6',
+    },
+    Medium: {
+      fontFamily: 'Pretendard',
+      fontSize: '18px',
+      fontWeight: 500,
+      lineHeight: '1.6',
+    },
+    Body: {
+      SemiBold: {
+        fontFamily: 'Pretendard',
+        fontSize: '16px',
+        fontWeight: 600,
+        lineHeight: '1.6',
+      },
+      Medium: {
+        fontFamily: 'Pretendard',
+        fontSize: '16px',
+        fontWeight: 500,
+        lineHeight: '1.6',
+      },
+      Regular: {
+        fontFamily: 'Pretendard',
+        fontSize: '16px',
+        fontWeight: 400,
+        lineHeight: '1.6',
+      },
+    },
+    Caption: {
+      SemiBold: {
+        fontFamily: 'Pretendard',
+        fontSize: '14px',
+        fontWeight: 600,
+        lineHeight: '1.6',
+      },
+      Medium: {
+        fontFamily: 'Pretendard',
+        fontSize: '14px',
+        fontWeight: 500,
+        lineHeight: '1.6',
+      },
+      Regular: {
+        fontFamily: 'Pretendard',
+        fontSize: '14px',
+        fontWeight: 400,
+        lineHeight: '1.6',
+      },
+    },
+  },
+};
+
+export default typo;
