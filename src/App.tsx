296d9a2989e5c6663e92dbf4825b81ac5d9b2cff
II-122 <76612998+II-122@users.noreply.github.com>
2024-08-24 18:46:39 +0900

    fix: 라우터 버그 수정
    App.tsx 라우터 버그 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { Layout } from './layout/layout';
 import AlarmPage from './pages/AlarmPage/AlarmPage';
@@ -8,13 +8,15 @@ import MainPage from './pages/MainPage/MainPage';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/alarm" element={<AlarmPage />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="alarm" element={<AlarmPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
