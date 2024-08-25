b42f57f190f136ae9aa0cd0bf80e7ddeec21bd00
onejuneee <onejuneee721@gmail.com>
2024-08-26 06:09:22 +0900

    fix: storybook 에서 컴파일 시 컴파일러 중복 사용으로 발생하는 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import {VitePWA, VitePWAOptions} from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
+// Vite 설정 파일
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
-      }
-    } as VitePWAOptions)
+        enabled: true,
+      },
+    } as VitePWAOptions), // PWA 설정
   ],
-})
+});
