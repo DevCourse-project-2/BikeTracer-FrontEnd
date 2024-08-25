ff7b83a9a3a58aa44afd249bf18d292098cecd98
onejuneee <onejuneee721@gmail.com>
2024-08-26 06:26:40 +0900

    bug: storybook 관련 코드는 type 검사를 하지 않도록 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa';
 
-// Vite 설정 파일
 export default defineConfig({
   plugins: [
     react(),
@@ -11,6 +10,6 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
-    } as VitePWAOptions), // PWA 설정
+    } as VitePWAOptions),
   ],
 });
